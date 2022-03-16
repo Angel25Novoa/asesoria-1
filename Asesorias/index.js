c8ebb76fb22e8be848c2b894e4cd9115197b2b4b
Duendisky <jorge.250495@gmail.com>
2022-03-15 20:03:21 -0600

    primer commit

---
+++ Asesorias/index.js
@@ -0,0 +1,47 @@
+// let num
+// function printPar(num) {
+//     while(true){
+//         num = prompt('Cuantos pares quieres mostrar exit para salir')
+//         if(num === 'exit' || 0){
+//             break
+//         } else {
+//             if (isNaN(num)){
+//                 alert('Debes ingresar un valor numérico')
+//             } else {
+//                 for(let i = 1; i <= num * 2; i++){
+//                     if(i % 2 === 0){
+//                         console.log(i)
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// printPar()
+
+// let eleph
+// function printEleph(eleph) {
+//     while(true){
+//         eleph = prompt('Cuantos elefantes quieres columpiar exit para salir')
+//         if(eleph === 'exit'){
+//             break
+//         } else {
+//             if (isNaN(eleph)){
+//                 alert('Debes ingresar un valor numérico')
+//             } else {
+//                 for(let i = 0; i <= eleph; i++){
+//                 if(eleph === 1) {
+//                     console.log(i, 'elefante se columpiaba sobre la tela de una araña...')
+//                 } else {
+//                     console.log(i, 'elefantes se columpiaban sobre la tela de una araña...')
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// printEleph()
+
+// console.log('Buenos "dias"')
